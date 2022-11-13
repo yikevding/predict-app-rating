8ced25fe43f81b2f0711d261d3a3419026e625c2
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-11-13 16:46:49 -0500

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,16 @@
+# TODO:
+
+# 1. Data Cleaning
+#    Deal with records with null value 
+#    Verify (free - price) relationship: free <--> price = 0
+#    Verify (rating - rating count) relationship: rating count = 0 --> rating = 0
+
+# 2. Feature Extractions
+#    Length of application name
+#    Last updated x days ago
+#    Month released
+#    Year released
+
+# 3. One-hot Encoding
+#    Categorical: Category, Content Rating
+#    Boolean: Ad Supported, In App Purchases, Editors Choice
