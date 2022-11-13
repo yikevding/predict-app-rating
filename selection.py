7b725446ed8f36fbb554b5069165634c4eb7cea8
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-11-13 16:47:14 -0500

    Create selection.py

---
+++ selection.py
@@ -0,0 +1,12 @@
+# TODO:
+
+# 1. Intuitive Selection
+#    Selected (13): Category, Size, Installs, Price adjusted for Currency, Month Released, Year Released, Content Rating, Ad Supported, In App Purchases, Editors Choice, Name Length (derived), Last Update x days ago (derived)
+#    Dropped (12): App Name, App Id, Rating Count, Minimum Installs, Maximum Installs, Free, Minimum Android, Developer ID, Developer Website, Developer Email, Privary Policy, Scraper Time
+#    FIXME: whether to add Last Updated into feature space?
+
+# 2. Pearson Correlation
+#    Reduce multicollinearity by dropping correlated features
+
+# 3. Lasso Regression
+#    Obtain important features for target prediction
